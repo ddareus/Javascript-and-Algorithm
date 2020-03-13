300b8d9486af6ba45831b0b369619cc51413cdad
ddareus <dareusdaniel@gmail.com>
2020-03-13 15:51:40 -0400

    Javascript apps & projects

---
+++ Building-To-Do-App/js/app.js
@@ -0,0 +1,19 @@
+let ourForm = document.getElementById('ourForm');
+let ourField = document.getElementById('ourField');
+let ourList = document.getElementById('ourList');
+
+ourForm.addEventListener('submit', e => {
+	e.preventDefault();
+	createItem(ourField.value);
+});
+
+function createItem(x) {
+	let ourHTML = `<li>${x} <button onclick="deleteItem(this)" >delete</button> </li>`;
+	ourList.insertAdjacentHTML('beforeend', ourHTML);
+	ourField.value = '';
+	ourField.focus();
+}
+
+function deleteItem(elementToDelete) {
+	elementToDelete.parentElement.remove();
+}
